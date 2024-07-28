44f8ad1bc9e3c73b98f16f476482d284dd5074ff
ismail elgoufri <elgoufriismail@gmail.com>
2024-07-28 01:22:20 +0100

    first commit

---
+++ src/main/java/com/RBACSYSTEM/RBAC/entity/Test.java
@@ -0,0 +1,16 @@
+package com.RBACSYSTEM.RBAC.entity;
+
+
+public class Test {
+    int id;
+    String name;
+
+    public Test(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    @Override
+    public String toString(){
+        return "test : id = " + this.id + " | nom = " + this.name;
+    }
+}
